fix issue with neural network numerical stability

python/learn_model.py
@@ -18,12 +18,14 @@ class DispersionModel(torch.jit.ScriptModule):
     def __init__(self):
         super(DispersionModel, self).__init__()
         self.fc1 = torch.nn.Linear(4, 100)
-        self.fc2 = torch.nn.Linear(100, 2)
+        self.fc2 = torch.nn.Linear(100, 50)
+        self.fc3 = torch.nn.Linear(50, 2)
 
     @torch.jit.script_method
     def forward(self, x):
-        x = torch.nn.functional.relu(self.fc1(x)) 
-        x = torch.nn.functional.relu(self.fc2(x))
+        x = torch.clamp(self.fc1(x), min=0) 
+        x = torch.clamp(self.fc2(x), min=0)
+        x = torch.clamp(self.fc3(x), min=.0001) 
         return x
 
 x = torch.from_numpy(x)
@@ -33,12 +35,12 @@ device = torch.device("cpu")
 
 model = DispersionModel()
 model = model.double()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-7)
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-8, momentum=0.9)
 
 train = torch.utils.data.TensorDataset(x, y)
 train_loader = torch.utils.data.DataLoader(train, batch_size=32, shuffle=True)
 
-for epoch in range(20):
+for epoch in range(200):
     running_loss = 0.0
     for i, data in enumerate(train_loader, 0):
         inputs, outputs = data
@@ -55,9 +57,9 @@ for epoch in range(20):
         optimizer.step()
 
         running_loss += loss.item()
-        if i % 10 == 9:    # print every 2000 mini-batches
+        if i % 10 == 9:    # print every 10 mini-batches
             print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
+                  (epoch + 1, i + 1, running_loss / 10))
             running_loss = 0.0
 
 model.save("dispersion_model.pt")